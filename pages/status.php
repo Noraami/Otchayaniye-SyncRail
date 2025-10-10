5b7a35db838ce0d3c8602a43b08d877ee5b89f3d
Vinicius-11340 <vinicius_zakalusny@estudante.sesisenai.org.br>
2025-10-10 10:47:34 -0300

    .

--- pages/status.php
+++ pages/status.php
@@ -13,15 +13,18 @@ if (!isset($_SESSION["conected"]) || $_SESSION["conected"] != true) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <title>STATUS</title>
+    <title>Status</title>
 </head>
 
-<body>
-    <a href="../connections/exit.php">
-        <input type="button" value="Sair" event="../connections/exit.php" />
-    </a>
+<body class="backgroundf min-vh-100 d-flex flex-column justify-content-center">
+    <div class="container bgcont rounded">
+        <a href="../connections/exit.php">
+            <input type="button" value="Sair" event="../connections/exit.php" />
+        </a>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 
