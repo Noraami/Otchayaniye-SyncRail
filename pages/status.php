50bce265f76337b36eea89e6e2bf262f1553aaa2
Vinicius-11340 <vinicius_zakalusny@estudante.sesisenai.org.br>
2025-10-10 12:40:40 -0300

    Co-authored-by: Gabriela Schappo Pimentel <Noraami@users.noreply.github.com>

--- pages/status.php
+++ pages/status.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include("../lay/menu.php");
 
 if (!isset($_SESSION["conected"]) || $_SESSION["conected"] != true) {
     header("Location: ../index.php");
@@ -14,6 +15,7 @@ if (!isset($_SESSION["conected"]) || $_SESSION["conected"] != true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/status.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Status</title>
@@ -21,9 +23,8 @@ if (!isset($_SESSION["conected"]) || $_SESSION["conected"] != true) {
 
 <body class="backgroundf min-vh-100 d-flex flex-column justify-content-center">
     <div class="container bgcont rounded">
-        <a href="../connections/exit.php">
-            <input type="button" value="Sair" event="../connections/exit.php" />
-        </a>
+        <div class=""></div>
+        <div class="m-3 "></div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
